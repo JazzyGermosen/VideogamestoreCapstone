301e03abb0814afbf1c660d23766a81d557eae28
Jazz <jazzygermosen@gmail.com>
2025-12-13 20:34:01 -0500

    First Commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,39 +1,77 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
-public class CategoriesController
-{
+
+@RestController
+public class CategoriesController {
+
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-
     // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    @Autowired
+    public void catcontroller(CategoryDao categoryDao, ProductDao productDao){
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("/categories")
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        //creating a empty list to store objects in it
+        List<Category> categories = new ArrayList<>();
+
+        String sql = """
+                
+                Select
+                    categoryId
+                    name
+                    description
+                From
+                    categories;           
+                """;
+
+        try(
+                Connection conn = ;
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery();
+                ){
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return categoryDao.getAllCategories();
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("/categories/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -42,7 +80,7 @@ public class CategoriesController
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return productDao.getById(categoryId);
     }
 
     // add annotation to call this method for a POST action

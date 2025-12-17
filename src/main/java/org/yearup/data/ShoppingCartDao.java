4096f658f360b9182e5ff4cf5f0ea6eccc1996a1
Jazz <jazzygermosen@gmail.com>
2025-12-17 13:43:25 -0500

    Created the ShoppingCartDao and added logic into it

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,20 @@
 package org.yearup.data;
 
+import org.yearup.controllers.ShoppingCartController;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import java.math.BigDecimal;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    ShoppingCart getByOrderId(int orderId);
+    void addItemShoppingCart(int userId, ShoppingCartItem item);
+    void updateItemShoppingCart(int userId, ShoppingCartItem item);
+    void deleteShoppingCart(int userId);
+    BigDecimal getShoppingCartTotal(ShoppingCart shoppingCart);
+
 }
